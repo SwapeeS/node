171f64b4665c28a43110db17e9028a0193efd40f
Sonissk <kuldeepsoni@Kuldeeps-MacBook-Pro.local>
2023-07-02 17:36:23 +0530

    new

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// const data = require('./data')
-// console.log(data.z())
+const data = require('./data')
+console.log(data.z())
 
 const http = require('http')
 http.createServer((req, res) => {
-    res.end("<H1>Hello Kuldeep Soni</H1>")
+    res.write("<H1>Hello Kuldeep Soni</H1>")
+    res.end()
 }
-).listen(3000)  // 3000 is port number
+).listen(3001)  // 3000 is port number
