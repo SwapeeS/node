5fb2bb85deccfc0f559dcf3fa1d607b447884fd1
SwapeeS <kuldeepsoni3099@gmail.com>
2023-07-02 17:58:18 +0530

    package

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const data = require('./data')
-console.log(data.z())
 
-const http = require('http')
-http.createServer((req, res) => {
-    res.write("<H1>Hello Kuldeep Soni</H1>")
-    res.end()
-}
-).listen(3001)  // 3000 is port number
+const color = require('colors');
+console.log("Hello world!".rainbow)
